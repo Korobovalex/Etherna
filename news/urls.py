19bff4fa21d513ba2642e01531a2cfdd4405548f
korobov Alex <korobovalex@mail.ru>
2023-01-10 11:17:09 +0300

    News CRUD views&routes

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.NewsListView.as_view(), name='news'),
+    # Single post operations
+    path('add/', views.NewsCreateView.as_view(), name='add'),
     path('post/<str:slug>/', views.NewsDetailView.as_view(), name='post'),
+    path('post/<str:slug>/edit/', views.NewsEditView.as_view(), name='edit'),
+    path('post/<str:slug>/delete/', views.NewsDeleteView.as_view(), name='delete'),
+    # News operations
+
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
 from .models import Post, Category, Tag
 from django.db.models import F
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -35,3 +39,41 @@ class NewsDetailView(DetailView):
         self.object.save()
         self.object.refresh_from_db()
         return context
+
+
+class NewsCreateView(CreateView):
+    model = Post
+    template_name = 'news_form.html'
+    fields = '__all__'
+    # fields = ['title', 'tex]
+    success_url = reverse_lazy('news')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Add new post'
+        return context
+
+
+class NewsEditView(UpdateView):
+    model = Post
+    template_name = 'news_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('news')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edit post'
+        return context
+
+
+class NewsDeleteView(DeleteView):
+    model = Post
+    template_name = 'news_confirm_delete.html'
+    context_object_name = 'post'
+    success_url = reverse_lazy('news')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delete post'
+        return context
+
